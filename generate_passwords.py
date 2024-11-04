74266cd22a6262922d287890c6e1af86b2c5d61c
Zero0chill <exploited.0110@gmail.com>
2024-11-04 01:56:43 -0500

    Update generate_passwords.py

--- generate_passwords.py
+++ generate_passwords.py
@@ -1,7 +1,7 @@
 import itertools
 
 # Define your character set and length
-charset = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+charset = "abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" #I forgot to add symbols like !@#$%^&*()":><.,? but you can do that as you plz
 length = 4  # Change this to the desired password length
 
 # Generate combinations
